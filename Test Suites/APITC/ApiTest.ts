f526dbbca7e33bd19cb6042233a03de16db910e8
Jeevan 4 ru <Jeevan 4 ru@jeevan>
2019-02-03 00:00:38 +0530

    My first commit

---
+++ Test Suites/APITC/ApiTest.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ApiTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c90b8727-8423-4754-8b27-844fa0afc817</testSuiteGuid>
+   <testCaseLink>
+      <guid>89168f6d-e452-4ad1-b82d-fccd56c69520</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculatorcases/AdditionTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1769ec2f-b5cb-446c-a9ed-1f8924fec87d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserDetails/Httpmethodcase</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4a79043-b102-43e2-9e3e-0514ced5e3d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserDetails/UserDetailsTC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
